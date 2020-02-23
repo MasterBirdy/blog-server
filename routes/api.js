e005b0a1e7e5ae5df9043f1b08d76e5099a2d335
Matthew Ito <mito81@gmail.com>
2020-02-23 03:03:53 -0800

    added files

--- routes/api.js
+++ routes/api.js
@@ -58,7 +58,6 @@ router.post("/signup", [
                     if (err) {
                         res.json({ err });
                     }
-                    console.log(user);
                     res.status(200).json({
                         user: { ...user._doc },
                         message: "Success!"
@@ -240,8 +239,6 @@ router.delete("/post/:id/delete", verifyToken, function(req, res, next) {
                 return res.status(400).json({ message: "Error finding post" });
             }
             const post = results;
-            console.log(post.author._id);
-            console.log(req.userData._id);
             if (post.author._id.toString() !== req.userData._id.toString()) {
                 return res.status(401).json({
                     message:
@@ -292,7 +289,6 @@ router.post("/post/:id/addcomment", [
                 text: req.body.text,
                 author: req.body.author
             });
-            console.log(comment);
             comment
                 .save()
                 .then(() => {
@@ -300,10 +296,15 @@ router.post("/post/:id/addcomment", [
                         .limit(1)
                         .exec((err, results) => {
                             if (err) {
-                                reject(err);
+                                return res.status(400).json({
+                                    message: "Error finding post",
+                                    err
+                                });
                             }
                             if (results == null) {
-                                reject("Post could not be found");
+                                return res.status(400).json({
+                                    message: "Post couldn't be found"
+                                });
                             } else {
                                 results[0].comments.push(comment);
                                 results[0].save();
@@ -317,6 +318,49 @@ router.post("/post/:id/addcomment", [
                     return res
                         .status(400)
                         .json({ message: "Error with adding comment", err });
+                });
+        }
+    }
+]);
+
+router.put("/post/:id/edit", verifyToken, [
+    body("title")
+        .isLength({ min: 1 })
+        .trim()
+        .withMessage("You must put in a title"),
+    body("text")
+        .isLength({ min: 1 })
+        .trim()
+        .withMessage("You must put in some text"),
+    check("title").escape(),
+    check("text").escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({
+                message: "Errors with editting post",
+                errors: errors.array()
+            });
+        } else {
+            Post.find({ _id: req.params.id })
+                .limit(1)
+                .exec((err, results) => {
+                    if (err) {
+                        return res
+                            .status(400)
+                            .json({ message: "Error finding post", err });
+                    }
+                    if (results == null) {
+                        return res
+                            .status(400)
+                            .json({ message: "Post was not found" });
+                    } else {
+                        results[0].title = req.body.title;
+                        results[0].text = req.body.text;
+                        results[0].status = req.body.status;
+                        results[0].save();
+                        return res.status(200).json({ message: "Success" });
+                    }
                 });
         }
     }
--- server.js
+++ server.js
@@ -15,8 +15,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/", apiRouter);
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/", apiRouter);
+app.get(/.*/, (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 app.listen(5000, () => console.log("Server started on port 5000"));
